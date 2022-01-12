8e83f564629043b2cf19fa1d2d8fc01a307657cd
daboxinxin <xxx_cumt@163.com>
2022-01-12 11:58:33 +0800

    删除所有文件

--- CMake/FindFreeGlut.cmake
+++
@@ -1,4 +0,0 @@
-
-FIND_PATH(FreeGlut_INCLUDE_DIR NAMES GL HINTS E:/Code/CUDAStereoMatching/SDK/glut/include)
-FIND_PATH(FreeGlut_LIB_DIR NAMES freeglut.lib HINTS E:/Code/CUDAStereoMatching/SDK/glut/lib)
-SET(FreeGlut_LIBRARY ${FreeGlut_LIB_DIR}/freeglut.lib)
--- CMake/FindGLUT.cmake
+++
@@ -1,4 +0,0 @@
-
-FIND_PATH(GLUT_INCLUDE_DIR NAMES glut.h HINTS E:/Code/CUDAStereoMatching/SDK/glut/include)
-FIND_PATH(GLUT_LIB_DIR NAMES glut.lib HINTS E:/Code/CUDAStereoMatching/SDK/glut/lib)
-SET(GLUT_LIBRARY ${GLUT_LIB_DIR}/glut.lib)
